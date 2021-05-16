6028a03458313512119c8aae6fdc8994dec40210
haruki <haruki0905.main@gmail.com>
2021-05-16 17:43:55 +0900

    account rock add

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,7 +39,18 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'haruki0905.sub@gmail.com',
+    :password => 'haruki0905',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
